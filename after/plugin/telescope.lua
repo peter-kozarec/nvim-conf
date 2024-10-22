c9264045e8140fd55333f16e21286369e109e3f0
Peter Kozarec <peter.kozarec@fiserv.com>
2024-10-22 11:14:29 +0200

    Minor changes

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -4,3 +4,9 @@ vim.keymap.set('n', '<C-p>', builtin.git_files, {desc = 'Git file'})
 vim.keymap.set('n', '<leader>ps', function()
     builtin.grep_string({search = vim.fn.input("Grep > ")})
 end, {desc = 'Telescope grep'})
+
+require('telescope').setup({
+    defaults = {
+        file_ignore_patterns = { ".git/", "tags",  }
+    }
+})
